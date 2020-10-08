2bffc0e8abf6076413f525dce0fed39aa8b726c2
Elsa Fluss <elsafluss@gmail.com>
2020-10-08 16:25:53 -0600

    complete warm-up and meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return menuItemName = "Delicious " + name
+};
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+};
+
+function addIngredients(item, ingredients) {
+  if(ingredients.includes(item)) {
+    return ingredients
+  } else {
+    return ingredients.push(item)
+  }
 }
 
+function formatPrice(price) {
+  return ("$" + price)
+}
+
+function decreasePrice(price) {
+  return (price * .9)
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
