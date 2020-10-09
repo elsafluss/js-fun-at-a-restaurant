0705a15caa50c77568f4e77eb35a3813d2a90d2f
Elsa Fluss <elsafluss@gmail.com>
2020-10-09 17:37:21 -0600

    complete order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,37 @@
+function takeOrder(newOrder, orderList) {
+  if(orderList.length >= 3) {
+    orderList
+  } else {
+    orderList.push(newOrder)
+  }
+}
+
+function refundOrder(removeOrder, orderNumber) {
+  orderNumber.splice((removeOrder - 1), 1)
+}
+
+function listItems(deliveryOrders, orderItemName) {
+  var output = ""
+  for(var i = 0; i < deliveryOrders.length - 1; i++) {
+    var currentElement = deliveryOrders[i].item
+    output += (currentElement + ", ")
+  }
+  output += deliveryOrders[deliveryOrders.length - 1].item
+  return output
+} //array.join, array.split
+
+function searchOrder(orderList, orderName) {
+  for(var i = 0; i < orderList.length; i++) {
+    if(orderList[i].item === orderName) {
+      return true
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
