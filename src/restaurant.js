5d17a0787481e1c235369439a1a67cf1fa1859d1
Elsa Fluss <elsafluss@gmail.com>
2020-10-11 11:44:58 -0600

    complete restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,33 @@
+function createRestaurant(restaurantNameValue) {
+  var myRestaurant = {
+    name: restaurantNameValue,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
+  return myRestaurant
+}
 
+function addMenuItem(myRestaurant, menuItemName) {
+  if(myRestaurant.menus[menuItemName.type].includes(menuItemName) === false) {
+    myRestaurant.menus[menuItemName.type].push(menuItemName)
+  }
+  return myRestaurant
+}
+
+function removeMenuItem(myRestaurant, menuItemName, menuType) {
+  if(myRestaurant.menus[menuType].some((element) => element.name === menuItemName)) {
+    myRestaurant.menus[menuType].splice(menuItemName.type, 1)
+    return `No one is eating our ${menuItemName} - it has been removed from the ${menuType} menu!`
+  } else {
+    return `Sorry, we don't sell ${menuItemName}, try adding a new recipe!`
+  }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
