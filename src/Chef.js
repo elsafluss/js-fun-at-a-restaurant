f49fd5ecbbefc1256bb9a83997ec91f89130e7e4
Elsa Fluss <elsafluss@gmail.com>
2020-10-11 11:43:32 -0600

    complete chef

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,38 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  // function createRestaurant(restaurantNameValue) {
+  //   var myRestaurant = { **this.restaurant**
+  //     name: restaurantNameValue,
+  //     menus: {
+  //       breakfast: [foodItem.something],
+  //       lunch: [],
+  //       dinner: [],
+  //     },
+  //   }
+  //   return myRestaurant
+  // }
+  // function addMenuItem(myRestaurant, menuItemName) {
+  //   if(myRestaurant.menus[menuItemName.type].includes(menuItemName) === false) {
+  //     myRestaurant.menus[menuItemName.type].push(menuItemName)
+  //   }
+  //   return myRestaurant
+  // }
+  greetCustomer(customerName, isMorning) {
+    if(isMorning) {
+      return `Good morning, ${customerName}!`
+    }
+    return `Hello, ${customerName}!`
+  }
+  checkForFood(foodItem) {
+    if(this.restaurant.menus[foodItem.type].includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    }
+    return `Sorry, we aren't serving ${foodItem.name} today.`
   }
 }
+
 
 module.exports = Chef;
